bdc32b2727e80b67efde7e76cc0e8ca924adb007
Topin <ltopin@gmail.com>
2019-03-21 19:57:17 -0300

    cadasto primeiro usuário e conexão com o banco

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
+//load User Model
+
+const User = require("../../models/User");
 //@Route    GET /api/users/teste
 //@desc     Test Users route
 //@access   Public
@@ -8,5 +13,39 @@ const router = express.Router();
 router.get("/teste", (req, res) =>
   res.json({ msg: "Funcionou a rota USERS !!" })
 );
+
+//@Route    GET /api/users/register
+//@desc     Register user
+//@access   Public
+
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Este email já existe !" });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", //tamanho da imagem
+        r: "pg", //rating
+        default: "mm" //default
+      });
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
 
 module.exports = router;
